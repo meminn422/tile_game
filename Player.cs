fc8f3ae197fc3915918e27541f3c7b49ee433872
Evyn Kim <129407119+meminn422@users.noreply.github.com>
2024-12-16 23:45:44 +0800

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -7,12 +7,14 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb; // Rigidbody2D 組件的引用
     public Animator animator; // Animator 組件的引用
     public SpriteRenderer spriteRenderer; // SpriteRenderer 組件的引用
-    public GameObject jc;
-    public LayerMask ground;
+    public GameObject jc; //empty object
+    public LayerMask ground; //map
     public float speed = 5f; // 水平移動速度
     public float jumpSpeed = 15f; // 垂直跳躍速度
-    private bool isJumping = false; // 用來檢查玩家是否在跳躍
-
+    public bool isJump; // 用來檢查玩家是否在跳躍
+    public bool isJumpCs=true;
+    public int jumpIndex=1;
+    public int jumpIndexcs=1;
     public bool isGround;
 
     private void Awake()
@@ -26,18 +28,14 @@ public class Player : MonoBehaviour
     private void Update()
     {
         // 檢查是否按下跳躍按鍵
-        if (Input.GetButtonDown("Jump")) // 修正了 GetButtonDown 的拼寫錯誤
+        if (Input.GetButtonDown("Jump")) 
         {
-            // if (Mathf.Abs(rb.velocity.y) < 0.01f) // 確保只有在玩家接觸地面時才能跳躍
-            // {
-                isJumping = true;
-            // }
-            isGround = Physics2D.OverlapCircle(jc.transform.position, 0.1f, ground);
+            isJump = true;            
         }
-
+        isGround = Physics2D.OverlapCircle(jc.transform.position, 0.1f, ground);
         // 獲取水平輸入
         float move = Input.GetAxisRaw("Horizontal");
-        
+
         // 設置 Animator 的 "Speed" 參數，用於控制動畫
         animator.SetFloat("Speed", Mathf.Abs(move));
 
@@ -57,21 +55,33 @@ public class Player : MonoBehaviour
         // 控制水平移動
         float move = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        animator.SetFloat("idle", Mathf.Abs(move));
+        spriteRenderer.flipX = move == 0 ? spriteRenderer.flipX : (move > 0 ? false : true);
 
+
+        if(isGround){
+            animator.SetBool("fall", false);
+            animator.SetBool("jump", false);
+            jumpIndexcs=jumpIndex;
+        }
         // 處理跳躍邏輯
-        if (isJumping)
-        {
-            animator.SetBool("jump",true);
-            animator.SetBool("fall",false);
+        if (isJump && jumpIndexcs > 0)
+        { 
+            
+            animator.SetBool("jump", true);
+            animator.SetBool("fall", false);
 
-            isJumping = false;
+            jumpIndexcs--;
+
+            isJump = false;
+            isGround=false; 
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed); // 應用跳躍的垂直速度
         }
-        if (rb.velocity.y<0)
-         {
-            animator.SetBool("fall",true);
-            animator.SetBool("jump",false);
+        if (rb.velocity.y < 0)
+        {
+            animator.SetBool("fall", true);
+            animator.SetBool("jump", false);
 
-         }
+        }
     }
 }
